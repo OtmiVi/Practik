7768821f50961329994f93243d55209db9817fad
Yura Kyziv <37220376+OtmiVi@users.noreply.github.com>
2022-02-06 15:08:21 +0200

    init

---
+++ storage/framework/views/3773abe8857b1c91edf5f1a0458c97fdbdab0651.php
@@ -0,0 +1,17 @@
+<?php $__env->startSection('content'); ?>
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
+                    Блог з використанням Laravel
+                </h1>
+                <a 
+                    href="/blog"
+                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                    Пости блогу
+                </a>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\Programs\OpenServer\domains\laravel-8-complete-blog-main\resources\views/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/4bf373879317d3bb34ba9d643184a4adfee19638.php
@@ -0,0 +1,28 @@
+<footer class="bg-gray-800 py-7 mt-0">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                <a href="/">
+                    Домашня сторінка
+                </a>
+            </h3>
+        </div>
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                <a href="/blog">
+                    Блог
+                </a>
+            </h3>
+        </div>
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                <a href="/login">
+                    Логін
+                </a>
+            </h3>
+        </div>
+    </div>
+    <p class="w-45 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-7">
+        КІ-20-1к Кузів Юрій
+    </p>
+</footer><?php /**PATH E:\Programs\OpenServer\domains\laravel-8-complete-blog-main\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
